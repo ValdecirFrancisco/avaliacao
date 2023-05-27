c6cf7d72287e746092aae3db20300dffd8a89431
ValdecirFrancisco <133457265+ValdecirFrancisco@users.noreply.github.com>
2023-05-27 00:55:55 +0000

    CORSE 18

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental:{
-        appDir:true,
-        serverActions: true,
-    },
+const nextConfig = {    
     async headers() {
     return [
       {
