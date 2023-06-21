db1802d6a54a2ecf196af95e44091d65cf7335aa
ValdecirFrancisco <133457265+ValdecirFrancisco@users.noreply.github.com>
2023-06-21 12:07:48 +0000

    feat: ataulização cadastro de disciplina

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
             <h2>LOGO</h2> <h2>Blog</h2> <h2>Biblioteca</h2>
             <h2>
 
-           --icon avatar--
+           
                 <a href="#" className="relative inline-flex items-center justify-center w-12 h-12 text-lg text-white rounded bg-emerald-500">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" aria-labelledby="title description" role="graphics-symbol">
                         <title id="title">User Icon</title>
